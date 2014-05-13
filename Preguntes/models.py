f979628ad960e1b0450c3159bee5afdf88ce0567
Juli <juli.fer14@gmail.com>
2014-05-13 19:48:18 +0200

    Commit dia 2

--- Preguntes/models.py
+++ Preguntes/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class tipusPregunta(models.Model):
@@ -9,3 +9,26 @@ class tipusPregunta(models.Model):
     
     def __unicode__(self):
         return self.noms
+    
+class pregunta(models.Model):
+    enunciat =  models.CharField(max_length=500)
+    tipus = models.ForeignKey(tipusPregunta)
+    usuari = models.ForeignKey(User)
+    
+    def __unicode__(self):
+        return self.enunciat
+    
+class respostaUsuari(models.Model):
+    pregunta =  models.ForeignKey(pregunta)
+    usuari = models.ForeignKey(User)
+    respostaUsuari = models.CharField(max_length=500)
+    
+    def __unicode__(self):
+        return self.respostaUsuari
+    
+class preguntaErronea(models.Model):
+    pregunta = models.ForeignKey(pregunta)
+    usuariNotifica = models.ForeignKey(User)
+    data = models.DateField()
+    hora = models.TimeField()
+    
