3f0e4b89de58e0fddafe2cfe3a464d19613619ea
Juli <juli.fer14@gmail.com>
2014-05-13 18:17:33 +0200

    Provveeess

--- PreguntMatic/settings.py
+++ PreguntMatic/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Preguntes',
+    'Usuaris',
 )
 
 MIDDLEWARE_CLASSES = (
--- Preguntes/models.py
+++ Preguntes/models.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+
+class tipusPregunta(models.Model):
+    nom = models.CharField(max_length=100,unique= True)
+    enunciatModel = models.CharField(max_length=200)
+    
+    def __unicode__(self):
+        return self.noms
