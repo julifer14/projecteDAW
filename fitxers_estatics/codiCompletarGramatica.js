2b8fcbe9640320b0b519b7f173e4466803257f99
Juli <juli.fer14@gmail.com>
2014-05-21 19:52:15 +0200

    Commit final dia començat el tipus completar gramatica

--- fitxers_estatics/codiCompletarGramatica.js
+++ fitxers_estatics/codiCompletarGramatica.js
@@ -1,5 +1,24 @@
 window.onload = transformarText();
 
 function transformarText() {
+	var preguntetes = new Array();
+	var idPreguntes = new Array();
+	preguntes = document.getElementsByClassName('preguntes');
+	for ( i = 0; i < preguntes.length; i++) {
+		p = preguntes[i].innerHTML;
+		for(j=0;j<p.length;j++){
+			preguntetes = p.split(".");
+			
+		
+			//idPreguntes.push(preguntes[i].id);
+			//preguntetes.push(preguntes[i].innerHTML);
+		}
+	}
+	//tinc un array de preguntes gat | gata - gos | gossa
+	for(i=0;i<preguntetes.length;i++){
+		console.log(preguntetes[i]);
+	}
+	
+	
 	alert("completar gramatica");
 }
